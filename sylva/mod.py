e89ec9bdbece78183a54a542e431c8cbd1db6bdc
Charlie Gunyon <charlie@charlieg.net>
2023-03-08 15:51:33 +0100

    .

--- sylva/mod.py
+++ sylva/mod.py
@@ -18,19 +18,13 @@ class Mod:
     locations: list[Location] = field(default_factory=list)
     name: str
     requirements: dict[str, Req] = field(default_factory=dict)
-    type_defs: dict[str, TypeDef] = field(init=False, default_factory=dict)
-    defs: dict[str, Union[ConstDef, SylvaDef]] = field(
+    defs: dict[str, Union[ConstDef, SylvaDef, TypeDef]] = field(
         init=False, default_factory=dict
     )
 
     def add_def(self, d: Union[ConstDef, SylvaDef, TypeDef]):
         if preexisting := builtins.lookup(d.name):
             raise errors.RedefinedBuiltIn(d.location, d.name)
-
-        if preexisting := self.type_defs.get(d.name):
-            raise errors.DuplicateDefinition(
-                d.location, d.name, preexisting.location
-            )
 
         if preexisting := self.defs.get(d.name):
             raise errors.DuplicateDefinition(
@@ -42,15 +36,9 @@ class Mod:
                 d.location, d.name, preexisting.location
             )
 
-        if isinstance(d, (ConstDef, SylvaDef)):
-            self.defs[d.name] = d
-        elif isinstance(d, TypeDef):
-            self.type_defs[d.name] = d
+        self.defs[d.name] = d
 
     def lookup(self, name):
-        return self.type_defs.get(
-            name,
-            self.defs.get(
-                name, self.requirements.get(name, builtins.lookup(name))
-            )
+        return self.defs.get(
+            name, self.requirements.get(name, builtins.lookup(name))
         )
